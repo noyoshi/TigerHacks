b547d1c9fb7f59433190c8a5e915c6c11cec198f
camicarballo <ccarball@nd.edu>
2017-10-14 15:45:23 -0400

    main

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
 
-import make_db, filter_f, fact, article, source
+import make_db, filter_f, extraction
 
+url = "http://feeds.bbci.co.uk/news/rss.xml"
+
+#make_db()
+print "Making database... URL: {}".format(url)
+facts_db = make_db.make_db(url)
 #load_db()
+print "Loading Database..."
+facts_db = filter_f.load_db('facts_db.json')
 #get articles
+print "Scraping..."
+articles = []
+  #run a scraper to get new facts
 #extract facts
-#filter facts()
-#deliver report
+for article in articles:
+  print "extracting facts..."
+  facts = extraction.extractFacts(article)
+  #filter facts
+  for fact in facts:
+    print "filtering facts..."
+    report = filter_f.filter_facts(fact, facts_db)
+    #deliver report
+    print "Report: {}".format(report)
