c94898c2ffe66db9e5d07f93324fa8a4ecb1206a
camicarballo <ccarball@nd.edu>
2017-10-14 13:21:49 -0400

    make json facts db

--- make_db.py
+++ make_db.py
@@ -3,13 +3,19 @@
 
 from bbc_scraper import bbc_scraper
 from extraction import extractFacts
+import json
 
 def make_db(url):
 
-  articles = bbc_scraper(url)
+  articles = []
+#  articles = bbc_scraper(url)
+
+  facts_dict = {}
   
-  with open('facts_db.txt', 'w') as f:
-    for article in articles:
-      facts = extractFacts(article)
-      for fact in facts:
-        f.write(fact)
+  for article in articles:
+    facts = extractFacts(article)
+    for fact in facts:
+      facts_dict[fact.factHash] = fact
+
+  with open('facts_db.json', 'w') as f:
+    f.write(json.dumps(facts_dict))
