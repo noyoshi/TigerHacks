7935af4f6439c9ea047b43ec726ba5a870a84317
Doug Smith <dougsmith2015@gmail.com>
2017-10-14 14:10:59 -0400

    Small changes to pass tests

--- filter_f.py
+++ filter_f.py
@@ -1,8 +1,8 @@
 # Filter new facts based on previously existing facts in the database
-from article import Article
+from article import article
 from fact import Fact
 from source import Source
 import fact_comp_lib
 
 def filter_facts():
-  return some_stuff
+  return "some_stuff"
