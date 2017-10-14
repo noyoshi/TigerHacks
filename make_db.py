c943b20d2c16add12ba1af6a510e3c90f7221a74
camicarballo <ccarball@nd.edu>
2017-10-14 14:19:38 -0400

    filter & make_db

--- make_db.py
+++ make_db.py
@@ -5,6 +5,15 @@ from bbc_scraper import bbc_scraper
 from extraction import extractFacts
 import json
 
+def make_json(fact):
+  fact_json = {}
+  fact_json['published_date'] = str(fact.published_date)
+  fact_json['distilled_fact'] = fact.distilledFact
+  fact_json['fact_strings'] = fact.factStrings
+  fact_json['confidence'] = fact.confidence
+  fact_json['factHash'] = fact.factHash
+  return fact_json
+
 def make_db(url):
 
   articles = []
@@ -15,7 +24,8 @@ def make_db(url):
   for article in articles:
     facts = extractFacts(article)
     for fact in facts:
-      facts_dict[fact.factHash] = fact
+      fact_json = make_json(fact)
+      facts_dict[fact.factHash] = fact_json
 
   with open('facts_db.json', 'w') as f:
     f.write(json.dumps(facts_dict))
--- test_filter.py
+++ test_filter.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
-from filter_f import filter_facts
+
+import json
+import filter_f, make_db
+from fact import Fact
+
+#write random fact to database
+random = Fact()
+r_hash = {}
+r_hash[random.factHash] = make_db.make_json(random)
+with open('facts_db_t.json', 'w') as t:
+  t.write(json.dumps(r_hash))
+
+print "loading database..."
+facts_db = filter_f.load_db('facts_db_t.json')
+print "filtering facts..."
+filter_f.filter_facts(random, facts_db)
