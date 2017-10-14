0e15890885596743dd69b64a8dba90ad3c2358ce
silver <nyoshida@nd.edu>
2017-10-14 03:43:45 -0400

    links enabled

--- ap_scraper.py
+++ ap_scraper.py
@@ -7,9 +7,12 @@ from datetime import datetime
 import urllib
 from bs4 import BeautifulSoup as soup 
 from article import article
-def ap_scraper():
-    my_url = "http://feeds.bbci.co.uk/news/rss.xml" # URL of rss feed / whatever you need
+def ap_scraper(my_url):
+    '''
+    returns a list of article objects from the scraped AP news URL
+    '''
 
+    # my_url = "http://feeds.bbci.co.uk/news/rss.xml" # URL of rss feed / whatever you need
 
     uClient = urllib.urlopen(my_url)
     page_html = uClient.read()
@@ -19,18 +22,22 @@ def ap_scraper():
     titles = []
     descriptions = []
     dates = []
-
-    for item in page_soup.findAll('item'):
+    links = []
+    for item in page_soup.findAll('item'): # I can make this better space efficiency and time 
         titles.append(item.title.text.encode("utf-8"))
         descriptions.append(item.description.text.encode("utf-8"))
         dates.append(str(item.pubDate.text))
-    return [titles, descriptions, dates]
+        links.append(item.title.text)
+    l = [titles, descriptions, dates, links]
 
-l = ap_scraper()
-article_list = []
-for i in range(len(l[0])):
-    temp = article(l[0][i],l[1][i],l[2][i])
-    article_list.append(temp)
+    article_list = []
+    for i in range(len(l[0])):
+        temp = article(l[0][i],l[1][i],l[2][i],l[3][i])
+        article_list.append(temp)
+    
+    return article_list
+  
+
     # print l[0][i]
     # print l[1][i]
     # print l[2][i]
--- article.py
+++ article.py
@@ -3,11 +3,12 @@
 from datetime import datetime 
 
 class article(object):
-    def __init__(self, title, description, date, body=""):
+    def __init__(self, title, description, date, body="", link):
         self.title = title
         self.description = description
         self.date = date 
         self.body = body 
+        self.link = link 
     def get_title(self):
         return self.title
     def get_description(self):
@@ -16,6 +17,8 @@ class article(object):
         return self.date 
     def get_body(self):
         return self.body
+    def get_link(self):
+        return self.link 
     def __str__(self): # Please don't use this lol, just use the get functions and then print those 
         return self.get_title()+ " " + self.get_description()+ " " + self.get_body() + " " + self.get_date()
         
